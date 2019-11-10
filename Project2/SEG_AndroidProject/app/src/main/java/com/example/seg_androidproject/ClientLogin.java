60c1bd96bb5033fa3918d83ced05db925bb19c65
Kaishuo Wang <kwang126@uottawa.ca>
2019-11-10 15:57:18 -0500

    ciecleCI

---
+++ Project2/SEG_AndroidProject/app/src/main/java/com/example/seg_androidproject/ClientLogin.java
@@ -0,0 +1,20 @@
+package com.example.seg_androidproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class ClientLogin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_client_login);
+        Intent intent = getIntent();
+        String name = intent.getStringExtra("Name");
+        TextView textView = (TextView)findViewById(R.id.textView5);
+        textView.setText("Welcome " + name + "! You are logged in as patient");
+    }
+}
