50675d5317ce5de68a831499b28efedc920d19f5
Kaishuo Wang <kwang126@uottawa.ca>
2019-11-12 17:18:17 -0500

    Project3

---
+++ SEG_AndroidProject/app/src/main/java/com/example/seg_androidproject/editEmployeeProfiles.java
@@ -0,0 +1,14 @@
+package com.example.seg_androidproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class editEmployeeProfiles extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_employee_profiles);
+    }
+}
